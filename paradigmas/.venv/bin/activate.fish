413904c1f80d213250d1568b27717fc30ba16b5f
Catfallen <markimprogramador2006@gmail.com>
2024-10-25 21:33:54 -0300

    to do list py

--- paradigmas/.venv/bin/activate.fish
+++ paradigmas/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/marcos/Documents/trabalhos_facimp/.venv"
+set -gx VIRTUAL_ENV "/home/marcos/Documents/trabalhos_facimp/paradigmas/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
